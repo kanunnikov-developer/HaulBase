bb1eb62a6748bd42830e13f61ba15da1288cf630
kanunnikov-developer <kolapsinfo@mail.ru>
2024-12-24 15:42:00 +0300

    Added "Tariffs" page

--- src/page/Tariffs/Tariffs.tsx
+++ src/page/Tariffs/Tariffs.tsx
@@ -8,23 +8,116 @@ export const Tariffs = () => {
         <h1>Тарифы</h1>
         <p>Автоматическое продление можно отключить в любой момент</p>
       </div>
+
       <div className={styles.cards}>
         <div className={styles.card}>
-          <div>
-            <img
-              src="icon-card.jpg"
-              alt="icon"
-            />
-            <p>Базовый</p>
+          <div className={styles.title}>
+            <div className={styles.circle}></div>
+            <h2>Базовый</h2>
+          </div>
+          <div className={styles.text}>
+            <p>Автоматическое продление можно отключить в любой момент</p>
+          </div>
+          <div className={styles.price}>
+            <p>
+              199 ₽<span>/мес</span>
+            </p>
+          </div>
+          <div className={styles.border}></div>
+          <div className={styles.advantages}>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Размещение заказов</p>
+            </div>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Ускорение времени модерации</p>
+            </div>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Отображение рейтинга на заказе</p>
+            </div>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Личный чат с заказчиком</p>
+            </div>
           </div>
           <Button className={styles.btn}>Подключить</Button>
         </div>
-        <div className={styles.card}>
+
+        <div className={styles["accent-card"]}>
+          <div className={styles["accent-title"]}>
+            <div className={styles['accent-circle']}></div>
+            <h2>Стандартный</h2>
+          </div>
+          <div className={styles["accent-text"]}>
+            <p>Автоматическое продление можно отключить в любой момент</p>
+          </div>
+          <div className={styles.price}>
+            <p>
+              499 ₽<span>/мес</span>
+            </p>
+          </div>
+          <div className={styles.border}></div>
+          <div className={styles.advantages}>
+            <div className={styles["advantages-text-accent"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Все из тарифа “Базовый”</p>
+            </div>
+            <div className={styles["advantages-text-accent"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Ускорение времени модерации</p>
+            </div>
+            <div className={styles["advantages-text-accent"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Отображение рейтинга на заказе</p>
+            </div>
+            <div className={styles["advantages-text-accent"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Личный чат с заказчиком</p>
+            </div>
+            <div className={styles["advantages-text-accent"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Личный чат с заказчиком</p>
+            </div>
+          </div>
           <Button className={styles["btn-accent"]}>
             Попробовать 7 дней бесплатно
           </Button>
         </div>
+
         <div className={styles.card}>
+          <div className={styles.title}>
+            <div className={styles.circle}></div>
+            <h2>Расширенный</h2>
+          </div>
+          <div className={styles.text}>
+            <p>Автоматическое продление можно отключить в любой момент</p>
+          </div>
+          <div className={styles.price}>
+            <p>
+              1029 ₽<span>/мес</span>
+            </p>
+          </div>
+          <div className={styles.border}></div>
+          <div className={styles.advantages}>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Размещение заказов</p>
+            </div>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Ускорение времени модерации</p>
+            </div>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Отображение рейтинга на заказе</p>
+            </div>
+            <div className={styles["advantages-text"]}>
+              <img src="advantages-icon.svg" alt="icon" />
+              <p>Личный чат с заказчиком</p>
+            </div>
+          </div>
           <Button className={styles.btn}>Подключить</Button>
         </div>
       </div>
